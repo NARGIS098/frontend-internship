7b8169614efb38c27498a4776a516279fc323258
Nargis sheikh <nagissheikh909@gmail.com>
2026-08-20 15:26:29 +0500

    week2 day4

---
+++ app.js
@@ -0,0 +1,5 @@
+import { greet, square, cube } from "./utils.js";
+
+console.log(greet("Nargis"));
+console.log("Square:", square(5));
+console.log("Cube:", cube(3));
---
+++ utils.js
@@ -0,0 +1,11 @@
+export const greet = (name) => {
+    return `Welcome ${name}`;
+};
+
+export const square = (number) => {
+    return number * number;
+};
+
+export const cube = (number) => {
+    return number * number * number;
+};
